b65b9a92b91da6e8825b573793afe81de8494977
Tom Dirinck <tom.dirinck@gmail.com>
2021-07-06 17:46:01 +0200

    Added functions.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,34 @@ import TodoList from './components/TodoList';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [todos, setTodos] = useState([{ id: uuidv4(), value: "Hello", checked: false }, { id: uuidv4(), value: "World", checked: true }])
+  const todoValue = useRef()
+
+  const handleTodo = () => {
+    const value = todoValue.current.value
+    if (value === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), value: value, checked: false }]
+    })
+    todoValue.current.value = null
+  }
+
+  const toggleTodo = (id) => {
+    const copy = [...todos]
+    const todo = copy.find(todo => todo.id === id)
+    todo.checked = !todo.checked
+    setTodos(copy)
+  }
+
+  useEffect(() => {
+    const todos = localStorage.getItem('todosApp.todos')
+    if (todos) setTodos(JSON.parse(todos))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todosApp.todos", JSON.stringify(todos))
+  }, [todos])
+
   return (
     <div className="App">
       <header className="App-header">
